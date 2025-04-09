a1ce96b01ffa84ecd4fa5aa531b6d32dc1b7e337
Hakan Bilgin <hbi99@hotmail.com>
2025-04-09 23:18:07 +0200

    animation tweaks

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -2,7 +2,7 @@
 let Test = {
 	init(APP) {
 		// auto stop Arena FPS control
-		setTimeout(() => APP.game.arena.fpsControl.stop(), 2e3);
+		// setTimeout(() => APP.game.arena.fpsControl.stop(), 2e3);
 
 		// backFlip backKick teaKick highKick midKick lowKick flyKick
 		setTimeout(() => APP.game.arena.player.move("flyKick"), 1e3);
