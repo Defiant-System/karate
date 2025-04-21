b1791b4aabd0558df9574c2bf1c19d8372ed0bba
Hakan Bilgin <hbi99@hotmail.com>
2025-04-21 12:18:46 +0200

    preparing for contact logic

--- src/js/modules/test.js
+++ src/js/modules/test.js
@@ -2,18 +2,20 @@
 let Test = {
 	init(APP) {
 		// auto stop Arena FPS control
-		setTimeout(() => APP.game.arena.fpsControl.stop(), 1e3);
+		// setTimeout(() => APP.game.arena.fpsControl.stop(), 1e3);
 
-		setTimeout(() => window.open("editor"), 300);
-		setTimeout(() => {
-			APP.editor.els.tree.find(`> .item:nth(2) > .icon-arrow`).trigger("click");
-			APP.editor.els.tree.find(`> .item:nth(2) > .children > .item:nth(20) > span`).trigger("click");
-		}, 800);
+		// setTimeout(() => window.open("editor"), 300);
+		// setTimeout(() => {
+		// 	APP.editor.els.tree.find(`> .item:nth(2) > .icon-arrow`).trigger("click");
+		// 	APP.editor.els.tree.find(`> .item:nth(2) > .children > .item:nth(20) > span`).trigger("click");
+		// }, 800);
 
 
 		// backFlip backKick teaKick highKick midKick lowKick flyKick
 		// setTimeout(() => APP.game.arena.player.move("backKick"), 1e3);
 		// setTimeout(() => APP.game.arena.player.keyUp("z"), 1e3);
+		setTimeout(() => APP.game.dispatch({ type: "window.keydown", char: "b" }), 1e3);
+		setTimeout(() => APP.game.dispatch({ type: "window.keyup", char: "b" }), 11e2);
 
 		// setTimeout(() => APP.dispatch({ type: "set-bg", arg: "bg-01.png" }), 500);
 		// setTimeout(() => APP.dispatch({ type: "set-bg", arg: "url('~/gfx/bg-02.webp')" }), 500);
